3944906e042dd1814339b578bee31a14874e367d
johnpark12 <loopuntiltrue@gmail.com>
2023-07-10 08:11:09 -0400

    Initial commit

---
+++ src/main/java/in/johnpark/dnsupdater/DnsupdaterApplication.java
@@ -0,0 +1,13 @@
+package in.johnpark.dnsupdater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnsupdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnsupdaterApplication.class, args);
+	}
+
+}
